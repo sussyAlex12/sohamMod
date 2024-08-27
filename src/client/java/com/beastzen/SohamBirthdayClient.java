4156ef241f10bfa684b13984a8aae91ecb70187e
sussyAlex12 <niggustusaugtus@gmail.com>
2024-08-27 22:15:21 +0530

    initialized project

---
+++ src/client/java/com/beastzen/SohamBirthdayClient.java
@@ -0,0 +1,10 @@
+package com.beastzen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SohamBirthdayClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
